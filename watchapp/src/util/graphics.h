d95c400b931ff334e82dd8681a2e133d0f3477b8
Matt Langlois <fletchto99@gmail.com>
2015-11-13 23:39:57 -0500

    Update pebble.js version

--- watchapp/src/util/graphics.h
+++ watchapp/src/util/graphics.h
@@ -15,10 +15,6 @@
 
 #endif
 
-static inline GPoint gpoint_neg(const GPoint a) {
-  return GPoint(-a.x, -a.y);
-}
-
 static inline GPoint gpoint_add(const GPoint a, const GPoint b) {
   return GPoint(a.x + b.x, a.y + b.y);
 }
